63efdf24b2ff3fb6b542f821e4460f9f10ec026e
Enayetur Rahman <anaytbd@outlook.com>
2025-07-23 16:36:51 +0600

    Add .next/static/chunks/src_app_admin_reports_page_tsx_a993527f._.js 1753267043259

---
+++ .next/static/chunks/src_app_admin_reports_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_734fdd47._.js",
+  "static/chunks/node_modules_2acb4809._.js"
+],
+    source: "dynamic"
+});
